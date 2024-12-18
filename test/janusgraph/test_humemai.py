312592b9d0eb6224522d51d19443fba00aec49d9
Taewoon Kim <tae898@gmail.com>
2024-12-18 19:00:06 +0100

    test: increase warmup time for Humemai instance in test setup

--- test/janusgraph/test_humemai.py
+++ test/janusgraph/test_humemai.py
@@ -12,7 +12,7 @@ class TestHumemai(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         """Start containers, connect to Gremlin, and initialize Humemai instance."""
-        cls.humemai = Humemai()
+        cls.humemai = Humemai(warmup_seconds=120)
         cls.humemai.connect()
         cls.humemai.remove_all_data()
 
